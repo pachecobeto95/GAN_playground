bfdbbc2bd10facd1181ad46fd9a9cd1dd0e44310
Lucas de Carvalho <Lucas-CG@users.noreply.github.com>
2021-01-17 18:53:51 -0300

    Update celebahq128.sh

--- celebahq128.sh
+++ celebahq128.sh
@@ -2,6 +2,6 @@
 for set in val train; do
   mkdir -p data/celeba_hq_128/$set/1
   for img in $(ls data/celeba_hq/$set | grep .jpg); do
-    convert data/celeba_hq/$set/1/$img -resize 128x128 data/celeba_hq_128/$set/1/$img &
+    convert data/celeba_hq/$set/$img -resize 128x128 data/celeba_hq_128/$set/1/$img &
   done
 done
