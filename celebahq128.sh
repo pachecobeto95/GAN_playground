041a3ba5c0af86818278e67643d24e284d2b3a39
Lucas de Carvalho <Lucas-CG@users.noreply.github.com>
2021-01-17 16:49:12 -0300

    Update celebahq128.sh

--- celebahq128.sh
+++ celebahq128.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 for set in val train; do
-  for gender in male female; do
-  mkdir -p data/celeba_hq_128/$set/$gender
-    for img in $(ls data/celeba_hq/$set/$gender | grep .jpg); do
-      convert data/celeba_hq/$set/$gender/$img -resize 128x128 data/celeba_hq_128/$set/$gender/$img &
-    done
+  mkdir -p data/celeba_hq_128/$set
+  for img in $(ls data/celeba_hq/$set | grep .jpg); do
+    convert data/celeba_hq/$set/$img -resize 128x128 data/celeba_hq_128/$set/$img &
   done
 done
