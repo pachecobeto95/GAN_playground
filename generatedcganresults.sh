1e36861fc1ad3d569d3c3ecc83564913349b37aa
Lucas de Carvalho <Lucas-CG@users.noreply.github.com>
2021-01-20 16:24:35 -0300

    Update generatedcganresults.sh

--- generatedcganresults.sh
+++ generatedcganresults.sh
@@ -5,5 +5,6 @@ cat logdcgan.txt | grep "Batch 937/938" | awk '{print $2}' | awk -F/ '{print $1}
 cat logdcgan.txt | grep "Batch 937/938" | awk '{print $7}' | awk -F] '{print $1}' > dlosses.txt
 cat logdcgan.txt | grep "Batch 937/938" | awk '{print $10}' | awk -F] '{print $1}' > glosses.txt
 cat logdcgan.txt | grep "Batch 937/938" | awk '{print $13}' | awk -F] '{print $1}' > totallosses.txt
-rm epoch.txt dlosses.txt glosses.txt totallosses.txt
 paste -d ',' epoch.txt dlosses.txt glosses.txt totallosses.txt >> results.csv
+rm epoch.txt dlosses.txt glosses.txt totallosses.txt
+
